3a9a391a794b47c5beb4cad69584e6a8b1a731d5
Riru1769 <canyoukindlykissmerightnow@gmail.com>
2025-05-18 01:30:11 -0700

    Create main.cpp
    mkk

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin>>name;
+     //name="fgdg";
+    cout << "Hello, " << name << "!" << endl;
+    return 0;
+}
